7c5ae9219d48aa944b0bda4b0079666748247c2a
PiotrMonczak <pmonczak@o2.pl>
2023-11-11 21:58:14 +0100

    Zadanie dzień 4.

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Witam wszystkich!");
+﻿
+string name = "Ania";
+var wiek = 25;
+string gender = "kobieta";
+
+if (gender == "kobieta" && wiek < 30)
+{
+    Console.WriteLine(" (1) - Kobieta poniżej 30 lat");
+}
+else if ( name == "Ewa" && wiek == 33)
+{
+    Console.WriteLine(" (2) - Ewa lat 33");
+}
+else if ( gender == "mezczyzna" && wiek < 18)
+{
+    Console.WriteLine(" (3) - Niepełnelotni mężczyzna");
+}
+else 
+{
+    Console.WriteLine(" (4) Warunki nie spełnione ");
+}
+
