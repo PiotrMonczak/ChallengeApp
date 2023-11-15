9b3b8ea5c7e4ee58d3beb869a49bb8a32f9a6193
PiotrMonczak <pmonczak@o2.pl>
2023-11-15 15:41:15 +0100

    Dzień 6

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,72 +1,40 @@
 ﻿
-var number = 344268;
-string numberInString = number.ToString();
-char[] letters = numberInString.ToArray();
 
-int counter0 = 0;
-int counter1 = 0;
-int counter2 = 0;
-int counter3 = 0;
-int counter4 = 0;
-int counter5 = 0;
-int counter6 = 0;
-int counter7 = 0;
-int counter8 = 0;
-int counter9 = 0;
+using ChallengeApp;
 
+Employee user1 = new Employee("Ania", "AniaAnia", "35");
+Employee user2 = new Employee("Tomek", "TomekTomek", "40");
+Employee user3 = new Employee("Kasia", "KasiaKasia", "25");
 
-foreach (char letter in letters)
+user1.AddScore(5);
+user1.AddScore(2);
+user1.AddScore(4);
+
+user2.AddScore(9);
+user2.AddScore(2);
+user2.AddScore(5);
+
+user3.AddScore(4);
+user3.AddScore(8);
+user3.AddScore(7);
+
+List<Employee> Workers = new List<Employee>() 
 {
-    if (letter == '0')
+    user1, user2, user3
+};
+
+int MaxResult = -1;
+Employee userWithMaxResult = null;
+
+foreach (var worker in Workers)
+{
+    if ( worker.Result1 > MaxResult) 
     {
-        counter0++;
-    }
-    else if (letter == '1')
-    {
-        counter1++;
-    }
-    else if (letter == '2')
-    {
-        counter2++;
-    }
-    else if (letter == '3')
-    {
-        counter3++;
-    }
-    else if (letter == '4')
-    {
-        counter4++;
-    }
-    else if (letter == '5')
-    {
-        counter5++;
-    }
-    else if (letter == '6')
-    {
-        counter6++;
-    }
-    else if (letter == '7')
-    {
-        counter7++;
-    }
-    else if (letter == '8')
-    {
-        counter8++;
-    }
-    else if (letter == '9')
-    {
-        counter9++;
+        userWithMaxResult = worker;
+        MaxResult = worker.Result1;
     }
 }
 
-Console.WriteLine("Dana liczbaLiczba 0 - " + number);
-Console.WriteLine("Liczba 0 - " + counter0);
-Console.WriteLine("Liczba 1 - " + counter1);
-Console.WriteLine("Liczba 2 - " + counter2);
-Console.WriteLine("Liczba 3 - " + counter3);
-Console.WriteLine("Liczba 4 - " + counter4);
-Console.WriteLine("Liczba 5 - " + counter5);
-Console.WriteLine("Liczba 6 - " + counter6);
-Console.WriteLine("Liczba 7 - " + counter7);
-Console.WriteLine("Liczba 8 - " + counter8);
-Console.WriteLine("Liczba 9 - " + counter9);
+
+Console.WriteLine(userWithMaxResult.name1);
+
---
+++ ChallengeApp/ChallengeApp/User.cs
@@ -0,0 +1,31 @@
+﻿
+namespace ChallengeApp
+{
+    public class Employee
+    {
+        private List<int> score = new List<int>();
+        public Employee(string name, string surname, string age)  // konstruktor
+        {
+            this.name1 = name;
+            this.surname1 = surname;
+            this.age1 = age;
+
+        }
+        public string name1 { get; set; } // property, pola
+        public string surname1 { get; set; }
+        public string age1 { get; set; }
+
+        public int Result1
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+
+        public void AddScore(int number) // metoda
+        {
+            this.score.Add(number);
+        }
+    }
+}
