24a740140e46747267d29d20186fa5a55b208549
PiotrMonczak <pmonczak@o2.pl>
2023-11-22 19:41:59 +0100

    Zadanie dzien 9 - Test

---
+++ ChallengeApp/ChallengeApp.Tests/EmployeeTests.cs
@@ -0,0 +1,59 @@
+﻿
+
+namespace ChallengeApp.Tests
+{
+    public class EmployeeTests
+    {
+        [Test]
+        public void ValueMax_ShouldBeEqual()
+        {
+            // arrange przygotowanie
+            var emp1 = new Employee("Ania", "AniaAnia");
+            emp1.AddGrade(4);
+            emp1.AddGrade(1);
+            emp1.AddGrade(7);
+
+            //act uruchomienie
+            var statistics = emp1.GetStatistics();
+            Assert.AreEqual(7, statistics.Max);
+
+            //assert sprawdzenie
+
+        }
+
+
+        [Test]
+        public void ValueMin_ShouldBeEqual()
+        {
+            // arrange przygotowanie
+            var emp1 = new Employee("Ania", "AniaAnia");
+            emp1.AddGrade(4);
+            emp1.AddGrade(1);
+            emp1.AddGrade(7);
+
+            //act uruchomienie
+            var statistics = emp1.GetStatistics();
+            Assert.AreEqual(1, statistics.Min);
+
+            //assert sprawdzenie
+
+        }
+
+        [Test]
+        public void ValueAverage_ShouldBeEqual()
+        {
+            // arrange przygotowanie
+            var emp1 = new Employee("Ania", "AniaAnia");
+            emp1.AddGrade(4);
+            emp1.AddGrade(1);
+            emp1.AddGrade(7);
+
+            //act uruchomienie
+            var statistics = emp1.GetStatistics();
+            Assert.AreEqual(4, statistics.Average);
+
+            //assert sprawdzenie
+
+        }
+    }
+}
